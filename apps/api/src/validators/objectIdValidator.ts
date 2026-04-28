f4232963f71f0e76b80c788e35f7efa5379ca41e
dimiona <dimiona@gmail.com>
2026-04-28 21:57:19 +0200

    Uses the common reponse function.

--- apps/api/src/validators/objectIdValidator.ts
+++ apps/api/src/validators/objectIdValidator.ts
@@ -1,6 +1,7 @@
 import { objectIdSchema } from "@repo/shared/schemas/scaffolding";
 import { validator } from "hono/validator";
 import z from "zod";
+import { invalidSchemaResponse } from "../utility/httpResponse.ts";
 
 const paramsWithIdSchema = z.object({
   id: objectIdSchema,
@@ -9,7 +10,7 @@ const paramsWithIdSchema = z.object({
 export const objectIdValidator = validator('param', (value, c) => {
   const result = paramsWithIdSchema.safeParse(value);
   if (!result.success) {
-    return c.json({ error: `Invalid response schema: ${z.treeifyError(result.error).errors.join('\n')}` }, 400);
+    return invalidSchemaResponse(c, "response", result.error, 400);
   }
 
   return result.data;
