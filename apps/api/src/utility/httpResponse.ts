491fe2f2734a548ca92db5558b52cca33b47867e
dimiona <dimiona@gmail.com>
2026-04-28 21:56:50 +0200

    Uses a more reliable response structure.

--- apps/api/src/utility/httpResponse.ts
+++ apps/api/src/utility/httpResponse.ts
@@ -4,17 +4,41 @@ import z from "zod";
 import type { $ZodError } from "zod/v4/core";
 
 export function successResponse<C extends Context>(c: C, responseData: any, statusCode: ContentfulStatusCode | undefined = 200) {
-  return c.json(responseData, statusCode);
+  return c.json(
+    {
+      success: statusCode >= 200 && statusCode < 300,
+      payload: responseData,
+    },
+    statusCode
+  );
 };
 
 export function errorResponse<C extends Context>(c: C, errors: string | string[], statusCode: ContentfulStatusCode | undefined = 500) {
-  return c.json({ error: Array.isArray(errors) ? errors.join("\n") : errors }, statusCode);
+  return c.json(
+    {
+      success: false,
+      error: Array.isArray(errors) ? errors.join("\n") : errors
+    },
+    statusCode
+  );
 };
 
 export function badRequestResponse<C extends Context>(c: C, errors: string | string[]) {
-  return c.json({ error: Array.isArray(errors) ? errors.join("\n") : errors }, 400);
+  return c.json(
+    {
+      success: false,
+      error: Array.isArray(errors) ? errors.join("\n") : errors
+    },
+    400
+  );
 };
 
 export function invalidSchemaResponse<C extends Context, E extends $ZodError<unknown>>(c: C, schemaName: string, errors: E, statusCode: ContentfulStatusCode | undefined = 400) {
-  return c.json({ error: `Invalid ${schemaName} schema: ${z.treeifyError(errors).errors.join('\n')}` }, statusCode);
+  return c.json(
+    {
+      success: false,
+      error: `Invalid ${schemaName} schema: ${z.treeifyError(errors).errors.join('\n')}`
+    },
+    statusCode
+  );
 };
