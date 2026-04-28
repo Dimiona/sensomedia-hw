4963675ca4f3e252cf2e9ef8af6cfd802aa274e3
dimiona <dimiona@gmail.com>
2026-04-28 11:39:12 +0200

    Event repository.

---
+++ apps/api/src/repositories/event.ts
@@ -0,0 +1,44 @@
+import { getDatabase } from "../services/database.ts";
+import type { TEventCreateSchema, TEventResponseSchema } from "../types/event.d.ts";
+
+class EventRepository {
+  collection() {
+    return getDatabase().collection("events");
+  }
+
+  async list(page: number = 1, perpage: number = 10) {
+    return this.collection()
+      .find<TEventResponseSchema>({})
+      .sort({
+        createdAt: -1,
+      })
+      .skip((page - 1) * perpage)
+      .limit(perpage)
+    ;
+  }
+
+  async create(event: TEventCreateSchema) {
+    const result = await this.collection().insertOne(event);
+    return {
+      ...event,
+      _id: result.insertedId,
+    };
+  }
+
+  async findById(id: TEventResponseSchema['_id']) {
+    return this.collection().findOne<TEventResponseSchema>({ _id: id });
+  }
+
+  async updateById(id: TEventResponseSchema['_id'], event: TEventCreateSchema) {
+    return this.collection().findOneAndUpdate(
+      { _id: id },
+      { $set: event }
+    );
+  }
+
+  async deleteById(id: TEventResponseSchema['_id']) {
+    return this.collection().findOneAndDelete({ _id: id });
+  }
+}
+
+export default new EventRepository();
