3ba13bdd5d45d0ec2d2d4fda2733f8e2abb7b088
dimiona <dimiona@gmail.com>
2026-04-29 10:21:31 +0200

    Fixes incorrect schema use. Also, added proper type to the POST request as a generic type.

--- apps/web/composables/api/booking.ts
+++ apps/web/composables/api/booking.ts
@@ -1,19 +1,19 @@
 import z from "zod";
-import type { TBookingCreateSchema } from "@repo/shared/types/booking.d";
-import type { IAPIRequests } from "../useApiService";
-import { eventCreateSchema } from "@repo/shared/schemas";
+import type { TBookingCreateSchema, TBookingResponseSchema } from "@repo/shared/types/booking.d";
+import { bookingCreateSchema } from "@repo/shared/schemas";
+import type { IAPIRequests } from "~/types/api";
 
 const namespace = "booking";
 
 export default ($request: IAPIRequests) => ({
 
   create(data: TBookingCreateSchema, idempotencyKey: string) {
-    const parsedData = eventCreateSchema.safeParse(data);
+    const parsedData = bookingCreateSchema.safeParse(data);
     if (!parsedData.success) {
       throw new Error(z.treeifyError(parsedData.error).errors.join(" "));
     }
 
-    return $request.post(`${namespace}`, data, undefined, { "Idempotency-Key": idempotencyKey });
+    return $request.post<TBookingResponseSchema>(`${namespace}`, data, undefined, { "Idempotency-Key": idempotencyKey });
   },
 
 });
