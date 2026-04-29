6ee7f06c863a2304f6f9942aca3f4247d678090e
dimiona <dimiona@gmail.com>
2026-04-29 10:23:01 +0200

    A loading overlay component.

---
+++ apps/web/components/LoadingOverlay.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label?: string;
+}>();
+</script>
+
+<template>
+  <div class="absolute flex items-center justify-center gap-2 size-full bg-[#ffffff68] backdrop-blur-[1px] text-sm font-medium">
+    <Icon name="i-lucide-loader" />
+    {{ props.label }}
+  </div>
+</template>
