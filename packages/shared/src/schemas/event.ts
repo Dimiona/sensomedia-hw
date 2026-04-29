141feea38e68577c6e0ec8a828eb4d8ecf44a5b2
dimiona <dimiona@gmail.com>
2026-04-29 10:10:44 +0200

    Adds new schema for /events API endpoint. Also, uses coerce for date prop.

--- packages/shared/src/schemas/event.ts
+++ packages/shared/src/schemas/event.ts
@@ -4,10 +4,18 @@ import type { ObjectId } from "bson";
 export const eventCreateSchema = z.strictObject({
   title: z.string().min(1),
   description: z.string().optional(),
-  date: z.date(),
+  date: z.coerce.date(),
   capacity: z.number().int().min(1),
 });
 
 export const eventResponseSchema = eventCreateSchema.extend({
   _id: z.custom<ObjectId>(),
+});
+
+export const eventsResponseSchema = z.strictObject({
+  items: z.array(eventResponseSchema),
+  metadata: z.object({
+    page: z.number().int().min(1),
+    perpage: z.number().int().min(1),
+  }),
 });
