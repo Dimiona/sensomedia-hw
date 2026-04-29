a88a09159756326bf8055d703bd1d7fa11f64d26
dimiona <dimiona@gmail.com>
2026-04-29 10:11:35 +0200

    Adds new inferred type.

--- packages/shared/src/types/event.d.ts
+++ packages/shared/src/types/event.d.ts
@@ -1,5 +1,6 @@
 import type z from "zod";
-import { eventCreateSchema, eventResponseSchema } from "@repo/shared/schemas/event";
+import { eventCreateSchema, eventResponseSchema, eventsResponseSchema } from "@repo/shared/schemas/event";
 
 export type TEventCreateSchema = z.infer<typeof eventCreateSchema>;
 export type TEventResponseSchema = z.infer<typeof eventResponseSchema>;
+export type TEventsResponseSchema = z.infer<typeof eventsResponseSchema>;
